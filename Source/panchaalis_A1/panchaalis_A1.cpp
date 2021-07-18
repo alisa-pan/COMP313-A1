69feba8484215b75d4eed402cfda3619f9718976
Alisa <panchaalis@myvuw.ac.nz>
2021-07-18 13:53:59 +1200

    Base code for a blank project with no starter content

---
+++ Source/panchaalis_A1/panchaalis_A1.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "panchaalis_A1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, panchaalis_A1, "panchaalis_A1" );
