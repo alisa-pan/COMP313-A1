10e0a9c2a5fbbcc5792e2c88db59dceba7d073fb
Alisa <panchaalis@myvuw.ac.nz>
2021-07-25 00:49:18 +1200

    Changed endscreen to display the winner of the game (the winner is whoever eats more apples, either the player/crow or the deer collectively).

--- Source/panchaalis_A1/MyCharacter.h
+++ Source/panchaalis_A1/MyCharacter.h
@@ -24,6 +24,9 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	float ApplesEaten;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool win = false;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
